866076bc639dd3862d2c2b9539b7e75f7b9373a2
scoful <1269717999@qq.com>
2024-08-06 15:15:51 +0800

    新增功能，换了个css主题

--- js/contentScript.js
+++ js/contentScript.js
@@ -4,6 +4,7 @@ console.log("linux_do_plus_content_script is done!!");
 document.addEventListener('DOMContentLoaded', function () {
     // 设置每秒（1000毫秒）调用一次
     setInterval(autoExpand, 1000);
+    injectCustomCss();
 });
 
 // 自动展开《贴中回复》
@@ -11,4 +12,17 @@ function autoExpand() {
     document.querySelectorAll('.fa.d-icon.d-icon-chevron-down.svg-icon.svg-node').forEach(function (element) {
         element.parentElement.click();
     });
+}
+
+// 插入美化css
+function injectCustomCss(cssPath) {
+    cssPath = cssPath || 'css/air.css';
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.href = chrome.runtime.getURL(cssPath);
+    link.onload = function () {
+        console.log("air.css loaded");
+    };
+    document.head.appendChild(link);
 }
